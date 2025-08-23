6da8a42852880a1d1c0c0f00192fdcf4c844697b
Uoh <166170062+UohMyGod@users.noreply.github.com>
2025-08-23 14:13:34 -0400

    Update content.js

--- content.js
+++ content.js
@@ -1,19 +1,25 @@
-setTimeout(() => {
-  // THE TWO HALVES, YKNOW
-  document.getElementById('loader').style.animation = 'none';
-  document.getElementById('topHalf').classList.add('fly-top');
-  document.getElementById('bottomHalf').classList.add('fly-bottom');
+window.onload = function() {
+  run();
+};
 
-  // BLOCKS
-  document.getElementById('topCurtain').classList.add('fly-curtain-top');
-  document.getElementById('bottomCurtain').classList.add('fly-curtain-bottom');
-
-  // MIDDLE FLASH THINGY
-  document.getElementById('staticCircle').classList.add('static-circle-flash');
-
-  // REMOVE LOADER
+function run() {
   setTimeout(() => {
-    document.getElementById('loaderOverlay').style.display = 'none';
-    document.body.style.overflow = 'auto';
-  }, 3500);
-}, 4000);
+    // THE TWO HALVES, YKNOW
+    document.getElementById('loader').style.animation = 'none';
+    document.getElementById('topHalf').classList.add('fly-top');
+    document.getElementById('bottomHalf').classList.add('fly-bottom');
+
+    // BLOCKS
+    document.getElementById('topCurtain').classList.add('fly-curtain-top');
+    document.getElementById('bottomCurtain').classList.add('fly-curtain-bottom');
+
+    // MIDDLE FLASH THINGY
+    document.getElementById('staticCircle').classList.add('static-circle-flash');
+
+    // REMOVE LOADER
+    setTimeout(() => {
+      document.getElementById('loaderOverlay').style.display = 'none';
+      document.body.style.overflow = 'auto';
+    }, 3500);
+  }, 4000);
+}
